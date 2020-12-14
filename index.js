e3d7663a53aa172568f7448967da20766b250fc8
daniel <carlos.daniel2016@hotmail.com>
2020-12-14 10:56:11 -0300

    Alteracoes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path')
 const { convert, toMoney } = require('./lib/convert')
-const { current } = require('./lib/getCurrentDollar')
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -9,8 +8,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', async (req, res) => {
-  const dollar = await current()
-  res.render('home', { dollar })
+  res.render('home')
 })
 
 app.get('/cotacao', (req, res) => {
